733b6687faf11055aea50c0052541ccb9a3d01f8
Tom Moya Schau <moya@asofterspace.com>
2021-01-27 09:01:53 +0100

    Add startup script

---
+++ startup.bat
@@ -0,0 +1,9 @@
+start "datacomx" C:\home\prog\delphi\datacomx\datacomx.bat
+start "Hugo" C:\home\prog\asofterspace\assSecretary\run.bat
+start "Mari" C:\home\prog\asofterspace\assAccountant\run.bat
+start "workbench" C:\home\prog\asofterspace\assWorkbench\run.bat
+start "browser" C:\home\prog\asofterspace\assBrowser\run.bat
+start "Zara" C:\home\prog\asofterspace\assTrainer\run.bat
+start "Fibo" C:\home\prog\asofterspace\assBusinessFinder\run.bat
+start "Towa" C:\home\prog\asofterspace\assDonkey\run.bat
+start "firefox" C:\Program Files\Mozilla Firefox\firefox.exe http://localhost:3012/
