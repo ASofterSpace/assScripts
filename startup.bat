f1b9221ca00bf5c5c82aed65693e7af70f8c3b60
Tom Moya Schau <moya@asofterspace.com>
2021-11-05 19:41:54 +0100

    Adjust startup timing

--- startup.bat
+++ startup.bat
@@ -1,14 +1,17 @@
 start "datacomx" C:\home\prog\delphi\datacomx\datacomx.bat
+
+timeout /t 1
+
 start "Hugo" C:\home\prog\asofterspace\assSecretary\run.bat
 start "Mari" C:\home\prog\asofterspace\assAccountant\run.bat
 start "workbench" C:\home\prog\asofterspace\assWorkbench\run.bat
 start "Towa" C:\home\prog\asofterspace\assDonkey\run.bat
 
-timeout /t 10
+timeout /t 15
 
 start "firefox" "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:3012/
 
-timeout /t 10
+timeout /t 5
 
 start "browser" C:\home\prog\asofterspace\assBrowser\run.bat
 start "Zara" C:\home\prog\asofterspace\assTrainer\run.bat
