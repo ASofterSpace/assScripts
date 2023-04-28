a2bd302a7fcc7019a547bae0f9b09c1cfe1decf3
Tom Moya Schau <moya@asofterspace.com>
2023-04-28 07:39:36 +0200

    Adjust startup order

--- startup.bat
+++ startup.bat
@@ -4,10 +4,10 @@ REM start "datacomx" C:\home\prog\delphi\datacomx\datacomx.bat
 
 REM timeout /t 1
 
+start "browser" C:\home\prog\asofterspace\assBrowser\run.bat
 start "Hugo" C:\home\prog\asofterspace\assSecretary\run.bat
 start "Mari" C:\home\prog\asofterspace\assAccountant\run.bat
 start "workbench" C:\home\prog\asofterspace\assWorkbench\run.bat
-start "Towa" C:\home\prog\asofterspace\assDonkey\run.bat
 
 timeout /t 15
 
@@ -15,7 +15,7 @@ start "firefox" "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:
 
 timeout /t 5
 
-start "browser" C:\home\prog\asofterspace\assBrowser\run.bat
 start "Zara" C:\home\prog\asofterspace\assTrainer\run.bat
 start "Fibo" C:\home\prog\asofterspace\assBusinessFinder\run.bat
 start "Fefi" C:\home\prog\asofterspace\assErrorFinder\run.bat
+start "Towa" C:\home\prog\asofterspace\assDonkey\run.bat
